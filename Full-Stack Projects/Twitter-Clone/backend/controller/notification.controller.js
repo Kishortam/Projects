70dd420fe6fd33e6d622846b7402a9b5919937f8
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-01-11 17:41:14 +0530

    1-update route files, paths & notification routes and controller added

---
+++ Full-Stack Projects/Twitter-Clone/backend/controller/notification.controller.js
@@ -0,0 +1,60 @@
+import Notification from "../model/notification.model.js";
+
+// get all notifications
+export const getNotifications = async(req, res) =>{
+    try {
+        const userId = req.user._id;
+
+        const notifications = await Notification.find({to:userId}).populate({
+            path: "from",
+            select: "username profileImg"
+        });
+
+        await Notification.updateMany({to: userId}, {read: true});
+
+        res.status(200).json(notifications);
+    } catch (error) {
+        console.log("Error in getNotification function", error.message);
+        res.status(500).json({error : "Internal server error"});
+    }
+};
+
+
+// delete notifications
+export const deleteNotifications = async(req, res) =>{
+    try {
+        const userId = req.user._id;
+
+        await Notification.deleteMany({to:userId});
+        
+    } catch (error) {
+        console.log("Error in deleteNotification function", error.message);
+        res.status(500).json({error : "Internal server error"});
+    }
+}
+
+
+// delete a single notification
+// export const deleteSingleNotification = async(req, res) =>{
+//     try {
+//         // get notification id
+//         const notificationId = req.params.id;
+//         const userId = req.user._id;
+//         // find notification with id
+//         const notification = await Notification.findById(notificationId);
+//         // if notify not found
+//         if(!notification){
+//             return res.status(404).json({error : "Notification not found"});
+//         }
+//         // check if user is allowed to delete notifi
+//         if(notification.to.toString() !== userId.toString()){
+//             return res.status(403).json({error: "You are not allowed to delete this notification"});
+//         }
+//         // delete notification
+//         await Notification.findByIdAndDelete(notificationId);
+//         res.status(200).json({message : "Notification deleted successfully"})
+//     } catch (error) {
+//         console.log("Error in deleteSingleNotification function", error.message);
+//         res.status(500).json({error : "Internal server error"});
+//     }
+// }
