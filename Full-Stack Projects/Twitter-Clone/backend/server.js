fd625e930ed6f3a8ff6c64bf529f2afe21c0194c
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-01-14 18:19:36 +0530

    3 - payload limit updated in server.js

--- Full-Stack Projects/Twitter-Clone/backend/server.js
+++ Full-Stack Projects/Twitter-Clone/backend/server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 // // to parse req.body
-app.use(express.json()); // middleware // runs betn req and res 
+app.use(express.json({limit: "5mb"})); // middleware // runs betn req and res  // 5mb is max size for images, limit should not be too high to prevent DoS
 app.use(express.urlencoded({extended: true})); // to parse form data (urlencoded)
 app.use(cookieParser());
 
