c1cb3038b0b568cf2326c399a6db5e39b5eaf0b7
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-01-15 15:34:55 +0530

    3 - Like post mutation added

--- Full-Stack Projects/Twitter-Clone/backend/controller/post.controller.js
+++ Full-Stack Projects/Twitter-Clone/backend/controller/post.controller.js
@@ -125,7 +125,9 @@ export const likeUnlikePost = async(req, res) =>{
         // unlike post
         await Post.updateOne({_id:postId}, {$pull: {likes: userId}});
         await User.updateOne({_id:userId}, {$pull: {likedPosts: postId}});
-        res.status(200).json({message: "Post unlike successfully"});
+
+        const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+        res.status(200).json(updatedLikes);
     }
     else{
         // like post
@@ -140,7 +142,8 @@ export const likeUnlikePost = async(req, res) =>{
         })
         await notification.save();
 
-        res.status(200).json({message: "Post liked successfully"});
+        const updatedLikes = post.likes;
+        res.status(200).json(updatedLikes);
     }
     } catch (error) {
         console.log("Error in likeUnlikePost controller: ", error);
--- Full-Stack Projects/Twitter-Clone/frontend/src/components/common/Post.jsx
+++ Full-Stack Projects/Twitter-Clone/frontend/src/components/common/Post.jsx
@@ -15,7 +15,7 @@ const Post = ({ post }) => {
 	const queryClient = useQueryClient();
 
 	// to delete post  // if post is our we can delete
-	const {mutate : deletePost, isPending} = useMutation({
+	const {mutate:deletePost, isPending:isDeleting} = useMutation({
 		mutationFn : async () => {
 			try {
 				const res = await fetch(`/api/posts/${post._id}`,{
@@ -36,10 +36,44 @@ const Post = ({ post }) => {
 			// invalidate the query to refetch the data
 			queryClient.invalidateQueries({queryKey : ["posts"]});
 		}
-	})
+	});
+
+	// like post
+	const {mutate:likePost, isPending:isLiking} = useMutation({
+		mutationFn : async () => {
+			try {
+				const res = await fetch(`/api/posts/like/${post._id}`,{
+					method : "POST",
+				})
+				const data = await res.json();
+	
+				if(!res.ok){
+				 throw new Error(data.error || "something went wrong");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess : (updatedLikes) =>{
+			// this is not the best UX, because it will refetch all posts
+			// instead, update the cache directly for that post
+			queryClient.setQueryData(['posts'], (oldData) =>{
+				return oldData.map((p) =>{
+					if(p._id === post._id){
+						return {...p, likes: updatedLikes};
+					}
+					return p;
+				})
+			});
+		},
+		onError : (error) =>{
+			toast.error(error.message);
+		}
+	});
 
 	const postOwner = post.user;
-	const isLiked = false;
+	const isLiked = post.likes.includes(authUser._id);
 	// if user is authenticated or post post belongs to him, it will thrash icon
 	const isMyPost = authUser._id === post.user._id;
 
@@ -55,7 +89,10 @@ const Post = ({ post }) => {
 		e.preventDefault();
 	};
 
-	const handleLikePost = () => {};
+	const handleLikePost = () => {
+		if(isLiking) return;
+		likePost();
+	};
 
 	return (
 		<>
@@ -77,9 +114,9 @@ const Post = ({ post }) => {
 						</span>
 						{isMyPost && (
 							<span className='flex justify-end flex-1'>
-								{!isPending && (<FaTrash className='cursor-pointer hover:text-red-500' 
+								{!isDeleting && (<FaTrash className='cursor-pointer hover:text-red-500' 
 								onClick={handleDeletePost} />)}
-								{isPending && ( <LoadingSpinner size="sm"/>)}
+								{isDeleting && ( <LoadingSpinner size="sm"/>)}
 							</span>
 						)}
 					</div>
@@ -95,6 +132,7 @@ const Post = ({ post }) => {
 					</div>
 					<div className='flex justify-between mt-3'>
 						<div className='flex gap-4 items-center w-2/3 justify-between'>
+						{/* by clicking on comment icon, we are opening comment dialog box */}
 							<div
 								className='flex gap-1 items-center cursor-pointer group'
 								onClick={() => document.getElementById("comments_modal" + post._id).showModal()}
@@ -146,11 +184,7 @@ const Post = ({ post }) => {
 											onChange={(e) => setComment(e.target.value)}
 										/>
 										<button className='btn btn-primary rounded-full btn-sm text-white px-4'>
-											{isCommenting ? (
-												<span className='loading loading-spinner loading-md'></span>
-											) : (
-												"Post"
-											)}
+											{isCommenting ? <LoadingSpinner size="md"/> : "Post"}
 										</button>
 									</form>
 								</div>
@@ -163,14 +197,15 @@ const Post = ({ post }) => {
 								<span className='text-sm text-slate-500 group-hover:text-green-500'>0</span>
 							</div>
 							<div className='flex gap-1 items-center group cursor-pointer' onClick={handleLikePost}>
-								{!isLiked && (
+								{isLiking && <LoadingSpinner size="sm"/>}
+								{!isLiked && !isLiking &&(
 									<FaRegHeart className='w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500' />
 								)}
-								{isLiked && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
+								{isLiked && !isLiking && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
 
 								<span
-									className={`text-sm text-slate-500 group-hover:text-pink-500 ${
-										isLiked ? "text-pink-500" : ""
+									className={`text-sm  group-hover:text-pink-500 ${
+										isLiked ? "text-pink-500" : "text-slate-500"
 									}`}
 								>
 									{post.likes.length}
