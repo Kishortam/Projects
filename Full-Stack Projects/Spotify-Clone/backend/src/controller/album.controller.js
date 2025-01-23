0c9ff3ebda7e685efba956c7371e215de2d16616
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-01-23 12:14:51 +0530

    album route and controller added

---
+++ Full-Stack Projects/Spotify-Clone/backend/src/controller/album.controller.js
@@ -0,0 +1,26 @@
+import Album from "../models/album.model.js";
+
+// to get all albums
+export const getAllAlbums = async(req, res, next) =>{
+    try {
+        const albums = await Album.find();
+        res.status(200).json(albums);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getAlbumId = async(req, res, next) =>{
+    try {
+        const {albumId} = req.params;  // get id of album
+        // find album by id and populate songs
+        const album = await Album.findById(albumId).populate("songs");
+
+        if(!album){  // if album not found
+            return res.status(404).json({message: "Album not found"});
+        }
+        res.status(200).json(album);  // else show album
+    } catch (error) {
+        next(error);
+    }
+};
--- Full-Stack Projects/Spotify-Clone/backend/src/routes/album.route.js
+++ Full-Stack Projects/Spotify-Clone/backend/src/routes/album.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { getAlbumId, getAllAlbums } from "../controller/album.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res)=>{
-    res.send("Happy Diwali");
-})
+router.get("/", getAllAlbums);
+router.get("/:albumId", getAlbumId);
 
 export default router;
