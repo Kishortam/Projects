4130f1fff1dbe18383370b9025738e434e441398
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-01-24 18:11:40 +0530

    Main layout and left sidebar added

--- Full-Stack Projects/Spotify-Clone/frontend/src/App.tsx
+++ Full-Stack Projects/Spotify-Clone/frontend/src/App.tsx
@@ -1,25 +1,31 @@
-import { Route, Routes } from "react-router-dom"
-import HomePage from "./pages/home/HomePage"
-import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage"
-import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
-
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
-
-
   return (
     <>
-    <Routes>
-      <Route path="/" element={<HomePage/>} />
+      <Routes>
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"}/>
+          }
+        />
 
-      <Route path="/sso-callback" 
-      element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"}/>}
-      />
-      
-      <Route path="/auth-callback" element={<AuthCallbackPage/>} />
-    </Routes>
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
+
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Full-Stack Projects/Spotify-Clone/frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,35 @@
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
+import { Outlet } from "react-router-dom"
+import LeftSidebar from "./components/LeftSidebar";
+
+const MainLayout = () => {
+    const isMobile = false;
+  return (
+    <div className="h-screen bg-black text-white flex flex-col">
+        <ResizablePanelGroup direction="horizontal" className="flex flex-1 h-full overflow-hidden p-2">
+            {/* left sidebar */}
+            <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
+                <LeftSidebar />
+            </ResizablePanel>
+
+            {/* to adjust length or witdth */}
+            <ResizableHandle className="w-2 bg-black rounded-lg transition-colors"/>
+
+            {/* main content */}
+            <ResizablePanel defaultSize={isMobile ? 80 : 60}>
+                <Outlet />
+            </ResizablePanel>
+
+            {/* to adjust length or witdth */}
+            <ResizableHandle className="w-2 bg-black rounded-lg transition-colors"/>
+
+            {/* right sidebar */}   
+            <ResizablePanel defaultSize={20} minSize={0} collapsedSize={0}>
+                friends activity
+            </ResizablePanel>
+        </ResizablePanelGroup>
+    </div>
+  )
+}
+
+export default MainLayout
