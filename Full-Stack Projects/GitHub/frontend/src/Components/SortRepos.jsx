eaddaf8ebcab9031233948b1f880aca6071ff645
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-02-01 15:53:36 +0530

    2- Sort Repos

--- Full-Stack Projects/GitHub/frontend/src/Components/SortRepos.jsx
+++ Full-Stack Projects/GitHub/frontend/src/Components/SortRepos.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 // Sorting repos based on sort type recent, forks, stars
 const SortRepos = ({onSort, sortType}) => { // imprting from homepage
+	// const BUTTONS = [
+	// 	{ type: "recent", text: "Most Recent" },
+	// 	{ type: "stars", text: "Most Stars" },
+	// 	{ type: "forks", text: "Most Forks" },
+	// ];
   return (
     <div className='mb-2 flex justify-center lg:justify-end'>
         {/* button 1 */}
 			<button
 				type='button'
-				className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+				className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass
+				${sortType === "recent" ? "border-blue-600" : ""}`}
 				onClick={()=> onSort("recent")}
 			>
 				Most Recent
@@ -15,7 +21,8 @@ const SortRepos = ({onSort, sortType}) => { // imprting from homepage
             {/* button 2 */}
 			<button
 				type='button'
-				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass
+				${sortType === "stars" ? "border-blue-600" : ""}`}
 				onClick={()=> onSort("stars")}
 			>
 				Most Stars
@@ -23,11 +30,31 @@ const SortRepos = ({onSort, sortType}) => { // imprting from homepage
             {/* button 3 */}
 			<button
 				type='button'
-				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass
+				${sortType === "forks" ? "border-blue-600" : ""}`}
 				onClick={()=> onSort("forks")}
 			>
 				Most Forks
 			</button>
+
+
+
+
+
+
+			{/* Optimized code for the above */}
+			{/* {BUTTONS.map((button) => (
+				<button
+					key={button.type}
+					type='button'
+					className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass ${
+						button.type == sortType ? "border-blue-500" : ""
+					}`}
+					onClick={() => onSort(button.type)}
+				>
+					{button.text}
+				</button>
+			))} */}
 		</div>
   )
 }
