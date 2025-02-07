af5d0b32d7c7a2309dac0d03058c459f402e839d
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-02-07 13:06:37 +0530

    2- Sort Repos added

--- Full-Stack Projects/GitHub-Clone/frontend/src/Components/ProfileInfo.jsx
+++ Full-Stack Projects/GitHub-Clone/frontend/src/Components/ProfileInfo.jsx
@@ -36,7 +36,7 @@ const ProfileInfo = ({userProfile}) => {
 					</a>
 					{/* View on Github */}
 					<div className='flex gap-2 items-center flex-col'>
-						{/* <LikeProfile user={userProfile} /> */}
+						
 						<a
 							href={userProfile?.html_url}
 							target='_blank'
