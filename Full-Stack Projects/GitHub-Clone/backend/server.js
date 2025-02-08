6612d68bafe18727ad5ec989e401d1f9bb1ad757
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-02-08 17:42:41 +0530

    4 - Package.json and Server.js updated before deploy

--- Full-Stack Projects/GitHub-Clone/backend/server.js
+++ Full-Stack Projects/GitHub-Clone/backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import passport from "passport";
 import session from "express-session";
+import path from "path";
 
 import "./Passport/github.auth.js";
 import authRoutes from "./Routes/auth.route.js";
@@ -13,6 +14,11 @@ import connectMongoDB from "./DB/connectMongoDB.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// to run client and server on same port
+const __dirname = path.resolve();
+console.log(__dirname);
 
 app.use(cors());
 
@@ -33,10 +39,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend","dist", "index.html"));
+});
 
 
 // server
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
     connectMongoDB();
 });
