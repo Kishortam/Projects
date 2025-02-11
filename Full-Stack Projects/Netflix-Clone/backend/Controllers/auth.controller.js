66c2933f1f24dc4ceedf9ca39e5c5f62cf577b69
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-02-11 17:48:28 +0530

    Backend Setup, Database MongoDB setup and Signup logic added

---
+++ Full-Stack Projects/Netflix-Clone/backend/Controllers/auth.controller.js
@@ -0,0 +1,73 @@
+import User from "../Models/user.model.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async(req, res) => {
+    try {
+        const {email, password, username} = req.body;
+        
+        // if not any of above is present
+        if(!email || !password || !username) {
+            return res.status(400).json({success: false, message: "All fields are required"});
+        }
+
+        // if email is not valid
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)) {
+            return res.status(400).json({success: false, message: "Invalid email"});
+        }
+
+        // if password length is less than 6
+        if(password.length < 6) {
+            return res.status(400).json({success: false, message: "Password must be at least 6 characters"});
+        }
+
+        // if email is already registered
+        const emailExists = await User.findOne({email});
+        if(emailExists) {
+            return res.status(400).json({success: false, message: "Email already exists"});
+        }
+
+        // if username is already registered
+        const userExists = await User.findOne({username});
+        if(userExists) {
+            return res.status(400).json({success: false, message: "User already exists"});
+        }
+
+
+        // hashing a password
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+
+        const PROFILE_PICS = ["/avatar1.png", "/avatar2.png", "/avatar3.png" ];
+        const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
+
+        // create a new user
+        const newUser = new User({
+            email,
+            password : hashedPassword, // so the password will be hashed
+            username,
+            image
+        });
+        // save user to database
+        await newUser.save();
+
+        // remove password from response
+        res.status(201).json({success: true, user:{
+            ...newUser._doc,
+            password: ""
+        }});
+    } catch (error) {
+        console.log("Error in signup: " + error.message);
+        res.status(500).json({success: false, message: "Internal server error"});
+    }
+}
+
+
+
+export const login = async(req, res) => {
+    res.send("login");
+}
+
+export const logout = async(req, res) => {
+    res.send("logout");
+}
---
+++ Full-Stack Projects/Netflix-Clone/backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./Routes/auth.route.js";
+import { connectDB } from "./DB/connectDB.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+// basic code to see on client or browser
+// app.get("/", (req, res) => {
+//     res.send("Hello from backend");
+// })
+
+app.use(express.json()); // middleware will allows us to parse req.body
+
+app.use("/api/v1/auth", authRoutes);
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+    connectDB();
+})
