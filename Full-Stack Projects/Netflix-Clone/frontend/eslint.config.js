04d6ae36b4e41a939ecf6a432f3d8c087c18202c
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-02-15 12:28:51 +0530

    Home Screen Builded

--- Full-Stack Projects/Netflix-Clone/frontend/eslint.config.js
+++ Full-Stack Projects/Netflix-Clone/frontend/eslint.config.js
@@ -3,6 +3,10 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tailwindScrollbarHide from 'tailwind-scrollbar-hide'
+
+
+tailwindScrollbarHide()
 
 export default [
   { ignores: ['dist'] },
