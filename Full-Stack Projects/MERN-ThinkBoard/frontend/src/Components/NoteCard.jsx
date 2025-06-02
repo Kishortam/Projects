6342a0e8f85c327399ccb4514e7c00d51000628a
Kishor Tambe <116078834+Kishortam@users.noreply.github.com>
2025-06-02 15:38:47 +0530

    Delete functionality added

--- Full-Stack Projects/MERN-ThinkBoard/frontend/src/Components/NoteCard.jsx
+++ Full-Stack Projects/MERN-ThinkBoard/frontend/src/Components/NoteCard.jsx
@@ -1,27 +1,50 @@
 import { PenSquareIcon, Trash2Icon } from 'lucide-react'
 import React from 'react'
 import { Link } from 'react-router'
+import api from '../Lib/axios'
+import toast from 'react-hot-toast'
 // import { formatDate } from '../Lib/utils'
 
-const NoteCard = ({note}) => {
-  return <Link to={`/note/${note._id}`}
-  className='card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4
-  border-solid border-[#00ff9d]'>
-    <div className='card-body'>
-      <h3 className='card-title text-base-content'>{note.title}</h3>
-      <p className='text-base-content/70 line-clamp-3'>{note.content}</p>
-      <div className='card-actions justify-between items-center mt-4'>
-        <span className='text-sm text-base-content/60'>{note.createdAt}</span>
-        <div className='flex items-center gap-1'>
-          <PenSquareIcon className='size-4' />
-          <button className='btn btn-ghost btn-xs text-error'>
-            <Trash2Icon className='size-4' />
-          </button>
+const NoteCard = ({note, setNotes}) => {
+
+  // delete the note
+  const handleDelete = async(e, id) => {
+    e.preventDefault(); // to get rid of the default behaviour of the browser
+
+    if(!window.confirm("Are you sure you want to delete this note?")) return;
+
+    try {
+      await api.delete(`/notes/${id}`);
+      setNotes((prevNotes) => prevNotes.filter((note) => note._id !== id)); // remove the note from the list & to update the UI immediately
+      toast.success("Note deleted successfully");
+    } catch (error) {
+      console.log(error, "Error deleting note");
+      toast.error("Error deleting note");
+    }
+  };
+
+  return (
+    <Link
+      to={`/note/${note._id}`}
+      className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4
+  border-solid border-[#00ff9d]"
+    >
+      <div className="card-body">
+        <h3 className="card-title text-base-content">{note.title}</h3>
+        <p className="text-base-content/70 line-clamp-3">{note.content}</p>
+        <div className="card-actions justify-between items-center mt-4">
+          <span className="text-sm text-base-content/60">{note.createdAt}</span>
+          <div className="flex items-center gap-1">
+            <PenSquareIcon className="size-4" />
+            <button className="btn btn-ghost btn-xs text-error" onClick={(e) => handleDelete(e, note._id)}>
+              <Trash2Icon className="size-4" />
+            </button>
+          </div>
         </div>
+        <p>{note.content}</p>
       </div>
-      <p>{note.content}</p>
-    </div>
-  </Link>
+    </Link>
+  );
 }
 
 export default NoteCard
--- Full-Stack Projects/MERN-ThinkBoard/frontend/src/Pages/HomePage.jsx
+++ Full-Stack Projects/MERN-ThinkBoard/frontend/src/Pages/HomePage.jsx
@@ -7,6 +7,7 @@ import Navbar from '../Components/Navbar'
 import RateLimitedUI from '../Components/RateLimitedUI';
 import NoteCard from '../Components/NoteCard';
 import api from '../Lib/axios';
+import NotesNotFound from '../Components/NotesNotFound';
 
 
 const HomePage = () => {
@@ -47,12 +48,14 @@ const HomePage = () => {
 
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
-        
 
+        {/* if no notes found */}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+        
         {notes.length > 0 && !isRateLimited && (
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
             {notes.map((note) => (
-              <NoteCard key={note._id} note={note} />
+              <NoteCard key={note._id} note={note} setNotes={setNotes}/>
             ))}
           </div>
         )}
